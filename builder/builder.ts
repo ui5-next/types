4ae4122fe43e153abd654b0780cae163b00a46fb
Theo Sun <theo.sun@sap.com>
2019-03-19 16:23:09 +0800

    feat: release info

--- builder/builder.ts
+++ builder/builder.ts
@@ -3,9 +3,10 @@ import { UI5APIRef, Kind } from './types';
 import { writeFileSync } from "fs";
 import * as path from "path";
 import { formatClassString } from './formatter';
+import * as fetch from "node-fetch";
 
 export const buildTypeDefination = (ref: UI5APIRef) => {
-  var typeString = "";
+  var typeString = `// UI5 Version: ${ref.version}\n// Date: ${new Date().toISOString()}\n`;
 
   ref.symbols.forEach(s => {
     if (s.kind == Kind.Class) {
@@ -14,5 +15,15 @@ export const buildTypeDefination = (ref: UI5APIRef) => {
   })
 
   writeFileSync(path.join(__dirname, "../bin/index.d.ts"), typeString, { encoding: "UTF-8" })
-
 }
+
+
+
+// MAIN process
+if (require.main === module) {
+  fetch(`https://openui5.hana.ondemand.com/test-resources/sap/ui/core/designtime/apiref/api.json`)
+    .then(res => res.json())
+    .then(buildTypeDefination)
+    .catch(console.error)
+}
+
